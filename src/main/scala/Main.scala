c29d9d45975672b1a8a99b0b49e56db81ecd93a1
lyalin <lyalin@retail-soft.pro>
2024-05-29 14:51:42 +0300

    Ленивая функция для вычисления заданного числа Фибоначчи

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,16 +1,17 @@
 object Main {
-
-  // Реализуйте MapReduce для подсчета частоты слов в тексте
-
-  private def map(text: String): Seq[(String, Int)] =
-    text.split("\\W+").filter(_.nonEmpty).map(w => (w.toLowerCase, 1))
-
-  private def reduce(pairs: Seq[(String, Int)]): Map[String, Int] =
-    pairs.groupBy(_._1).view.mapValues(_.map(_._2).sum).toMap
-
-  private def mapReduce(text: String): Map[String, Int] = reduce(map(text))
-
   def main(args: Array[String]): Unit = {
-    println(mapReduce("one two two three three three"))
+
+    // Напишите ленивую версию функции для вычисления n-го числа Фибоначчи
+
+    lazy val fibs: LazyList[Int] = {
+      def fibHelper(a: Int, b: Int): LazyList[Int] = {
+        a #:: fibHelper(b, a + b)
+      }
+      fibHelper(0, 1)
+    }
+
+    def fibonacci(n: Int): Int = fibs(n)
+
+    println(fibonacci(6))
   }
 }
