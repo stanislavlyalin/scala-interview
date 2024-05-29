680106cac4e670393552b724533b5fd88c646ad6
lyalin <lyalin@retail-soft.pro>
2024-05-29 14:36:34 +0300

    MapReduce для подсчёта частоты слов в тексте

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,43 +1,16 @@
 object Main {
 
-  // Создайте иерархию классов для геометрических фигур (круг, квадрат, прямоугольник) и напишите функцию,
-  // которая вычисляет площадь фигуры, используя pattern matching
+  // Реализуйте MapReduce для подсчета частоты слов в тексте
 
-  private sealed trait Shape
+  private def map(text: String): Seq[(String, Int)] =
+    text.split("\\W+").filter(_.nonEmpty).map(w => (w.toLowerCase, 1))
 
-  private case class Circle(r: Double) extends Shape
-  private case class Square(x: Double) extends Shape
-  private case class Rectangle(x: Double, y: Double) extends Shape
+  private def reduce(pairs: Seq[(String, Int)]): Map[String, Int] =
+    pairs.groupBy(_._1).view.mapValues(_.map(_._2).sum).toMap
 
-  private def area(shape: Shape): Double = shape match {
-    case Circle(r) => Math.PI * r * r
-    case Square(x) => x * x
-    case Rectangle(x, y) => x * y
-  }
-
-  private sealed trait ShapeTypeClass {
-    def area: Double
-  }
-
-  private case class CircleTypeClass(r: Double) extends ShapeTypeClass {
-    override def area: Double = Math.PI * r * r
-  }
-
-  private case class SquareTypeClass(x: Double) extends ShapeTypeClass {
-    override def area: Double = x * x
-  }
-
-  private case class RectangleTypeClass(x: Double, y: Double) extends ShapeTypeClass {
-    override def area: Double = x * y
-  }
-
-  private def areaTypeClass[A <: ShapeTypeClass](s: A): Double = s.area
+  private def mapReduce(text: String): Map[String, Int] = reduce(map(text))
 
   def main(args: Array[String]): Unit = {
-    println(area(Circle(10.0)))
-    println(area(Square(10.0)))
-    println(area(Rectangle(10.0, 20.0)))
-
-    println(areaTypeClass(CircleTypeClass(10.0)))
+    println(mapReduce("one two two three three three"))
   }
 }
