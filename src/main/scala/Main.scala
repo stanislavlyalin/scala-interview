a538f47e94b30945be079f3856b69f40fbecaebd
lyalin <lyalin@retail-soft.pro>
2024-05-29 14:04:33 +0300

    Подсчёт суммы массива в 4 параллельных потоках (Future). Какждый поток суммирует свой отдельный фрагмент массива (1/4)

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,20 +1,30 @@
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, Future}
+import scala.util.{Failure, Success}
+
 object Main {
 
-  // Реализуйте бесконечный стрим простых чисел
-
-  private def isPrime(n: Int): Boolean = {
-    if (n < 1) false
-    else if (n == 2) true
-    else !(2 until Math.sqrt(n).toInt + 1).exists(i => n % i == 0)
-  }
-
-  private def primes: LazyList[Int] = {
-    def nextPrime(current: Int): Int = LazyList.from(current + 1).filter(isPrime).head
-
-    LazyList.iterate(1)(nextPrime)
-  }
-
   def main(args: Array[String]): Unit = {
-    println(primes.take(10).toList)
+
+    // Напишите программу, которая считает сумму чисел в массиве, используя параллельные вычисления
+
+    val arr = (1 to 1000).toArray
+
+    val parallelSum: Future[Int] = Future.sequence(
+      Seq(
+        Future { arr.slice(0, 250).sum },
+        Future { arr.slice(250, 500).sum },
+        Future { arr.slice(500, 750).sum },
+        Future { arr.slice(750, 1000).sum }
+      )
+    ).map(_.sum)
+
+    parallelSum.onComplete {
+      case Success(sum) => println(sum)
+      case Failure(exception) => println(exception)
+    }
+
+    Await.result(parallelSum, 10.seconds)
   }
 }
