606dc0d5125bbeb5f22da68585a1254f2ee04a26
lyalin <lyalin@retail-soft.pro>
2024-05-29 13:21:42 +0300

    Сумма элементов списка с помощью рекурсии и хвостовой рекурсии

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,24 @@
+import scala.annotation.tailrec
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+
+    // Напишите функцию, которая принимает список чисел и возвращает их сумму, используя рекурсию и хвостовую рекурсию
+
+    def sum(nums: List[Int]): Int = {
+      nums match {
+        case h :: Nil => h
+        case h :: tail => h + sum(tail)
+      }
+    }
+
+    @tailrec
+    def sumTail(nums: List[Int], total: Int = 0): Int = {
+      if (nums.isEmpty) total else sumTail(nums.tail, total + nums.head)
+    }
+
+    val nums = List(1, 2, 3, 4, 5)
+    println(sum(nums))
+    println(sumTail(nums))
   }
 }
