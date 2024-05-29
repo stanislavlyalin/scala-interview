7280fab7aaf3c21674e2e2ef76f18ca1b480ff8e
lyalin <lyalin@retail-soft.pro>
2024-05-29 13:48:34 +0300

    Бесконечный стрим простых чисел

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,17 +1,20 @@
 object Main {
+
+  // Реализуйте бесконечный стрим простых чисел
+
+  private def isPrime(n: Int): Boolean = {
+    if (n < 1) false
+    else if (n == 2) true
+    else !(2 until Math.sqrt(n).toInt + 1).exists(i => n % i == 0)
+  }
+
+  private def primes: LazyList[Int] = {
+    def nextPrime(current: Int): Int = LazyList.from(current + 1).filter(isPrime).head
+
+    LazyList.iterate(1)(nextPrime)
+  }
+
   def main(args: Array[String]): Unit = {
-
-    // Используя монаду Option, напишите функцию, которая принимает два аргумента типа Option[Int]
-    // и возвращает их сумму, если оба аргумента определены
-
-    def sumOption(a: Option[Int], b: Option[Int]): Option[Int] = for {
-      aVal <- a
-      bVal <- b
-    } yield aVal + bVal
-
-    println(sumOption(Some(1), Some(2)))
-    println(sumOption(Some(1), None))
-    println(sumOption(None, Some(2)))
-    println(sumOption(None, None))
+    println(primes.take(10).toList)
   }
 }
