7f38ecbee374514b5d84fcc5cbbb00dce0707cd3
lyalin <lyalin@retail-soft.pro>
2024-05-29 13:35:45 +0300

    Функция, которая парсит строку в целое число и возвращает Option[Int]

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,23 +1,13 @@
+import scala.util.Try
+
 object Main {
-
-  // Реализуйте неизменяемую (immutable) очередь (queue) с методами enqueue и dequeue
-  class Queue[A] private (items: List[A]) {
-    def enqueue(item: A): Queue[A] = Queue[A](items :+ item)
-    def dequeue: (Option[A], Queue[A]) = items match {
-      case h :: tail => (Some(h), Queue[A](tail))
-      case Nil => (None, Queue[A]())
-    }
-    override def toString: String = s"Queue(${items.mkString(", ")})"
-  }
-
-  object Queue {
-    def apply[A]() = new Queue[A](List.empty[A])
-    def apply[A](items: List[A]) = new Queue[A](items)
-  }
-
   def main(args: Array[String]): Unit = {
-    val queue = Queue(List(1, 2, 3))
-    println(queue.enqueue(4))
-    println(queue.dequeue)
+
+    // Напишите функцию, которая парсит строку в целое число, возвращая Option[Int]
+
+    def toInt(s: String): Option[Int] = Try(s.toInt).toOption
+
+    println(toInt("555"))
+    println(toInt("test"))
   }
 }
