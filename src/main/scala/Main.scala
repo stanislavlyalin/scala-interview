989b7bf2dfc386913032ee14d5479470d301a1e6
lyalin <lyalin@retail-soft.pro>
2024-05-29 15:40:59 +0300

    Простой DSL для графа и функция поиска в графе

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,49 @@
 object Main {
+
+  // Создайте простой DSL для описания графов и напишите функции для поиска в графе
+
+  case class Node[A <: AnyVal](v: A, neighbours: List[Node[A]])
+
+  trait Graph[A <: AnyVal] {
+    def allNodes: List[Node[A]]
+    def addNode(node: Node[A]): Unit
+    def exists(v: A, from: Node[A]): Boolean
+  }
+
+  private class GraphImpl[A <: AnyVal] private (var nodes: List[Node[A]]) extends Graph[A] {
+    override def allNodes: List[Node[A]] = nodes
+    override def addNode(node: Node[A]): Unit = {
+      nodes = nodes.appended(node)
+    }
+
+    override def exists(v: A, from: Node[A]): Boolean = {
+      def recursiveExists(v: A, from: Node[A]): Boolean = {
+        if (from.neighbours.map(_.v).contains(v)) true
+        else from.neighbours.exists(node => recursiveExists(v, node))
+      }
+      recursiveExists(v, from)
+    }
+  }
+
+  private object GraphImpl {
+    def apply[A <: AnyVal] = new GraphImpl[Int](List.empty)
+    def apply[A <: AnyVal](nodes: List[Node[A]]) = new GraphImpl[A](nodes)
+  }
+
   def main(args: Array[String]): Unit = {
 
-    // Напишите универсальную функцию для слияния двух списков произвольного типа
+    val graph = GraphImpl[Int]
 
-    def concatenateLists[A, B](a: List[A], b: List[B]): List[Any] = a ++ b
+    val node1: Node[Int] = Node(1, List.empty)
+    graph.addNode(node1)
 
-    println(concatenateLists(List(1, 2, 3), List(4.0, 5.0)))
-    println(concatenateLists(List("a", "b", "c"), List(true, false)))
+    val node2 = Node(2, graph.allNodes)
+    graph.addNode(node2)
+
+    val node3 = Node(3, graph.allNodes)
+    graph.addNode(node3)
+
+    println(graph.exists(2, node1))  // false
+    println(graph.exists(1, node3))  // true
   }
 }
