b2d425fed7c4b040e995ef7999d33cb662094811
lyalin <lyalin@retail-soft.pro>
2024-06-11 10:02:03 +0300

    Иммутабельные и мутабельные типы

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,29 +1,60 @@
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
-import scala.util.{Failure, Success}
+import scala.collection.mutable
 
 object Main {
   def main(args: Array[String]): Unit = {
 
-    // Promise и Future
+    // Иммутабельные и мутабельные типы
 
-    // Promise - контейнер для Future. Так и не смог понять, в каких случаях Promise полезен
+    // Иммутабельные: не изменяются. Безопасно в многопоточных приложениях, но больше памяти и хуже производительность)
 
-    // Future - отложенное значение. Расчёт значения запускается в отдельном потоке
+    val list = List(1, 2, 3)
+    val newList = list :+ 4
 
-    Future {
-      Thread.sleep(1000)
-      println("Future завершена")
-      42
-    }.onComplete {
-      case Success(value) => println(s"Future успешно вернула $value")
-      case Failure(exception) => println(s"Ошибка в Future ${exception.getMessage}")
-    }
+    println(list) // Output: List(1, 2, 3)
+    println(newList) // Output: List(1, 2, 3, 4)
 
-    println("Выполняется сразу после создания Future")
+    val map = Map("a" -> 1, "b" -> 2)
+    val newMap = map + ("c" -> 3)
 
-    Thread.sleep(2000)
+    println(map) // Output: Map(a -> 1, b -> 2)
+    println(newMap) // Output: Map(a -> 1, b -> 2, c -> 3)
 
-    println("К этому моменту Future завершена и напечатала строку")
+    val set = Set(1, 2, 3)
+    val newSet = set + 4
+
+    println(set) // Output: Set(1, 2, 3)
+    println(newSet) // Output: Set(1, 2, 3, 4)
+
+    case class Person(name: String, age: Int)
+
+    val person = Person("Alice", 30)
+    val olderPerson = person.copy(age = 31)
+
+    println(person) // Output: Person(Alice,30)
+    println(olderPerson) // Output: Person(Alice,31)
+
+    // Мутабельные: изменяются. Производительнее и меньше памяти, но нужно синхронизировать в многопоточных приложениях
+
+    val array = Array(1, 2, 3)
+    array(0) = 10
+
+    println(array.mkString(", ")) // Output: 10, 2, 3
+
+    val listBuffer = mutable.ListBuffer(1, 2, 3)
+    listBuffer += 4
+
+    println(listBuffer) // Output: ListBuffer(1, 2, 3, 4)
+
+    val mutableMap = mutable.Map("a" -> 1, "b" -> 2)
+    mutableMap += ("c" -> 3)
+
+    println(mutableMap) // Output: Map(a -> 1, b -> 2, c -> 3)
+
+    class MutablePerson(var name: String, var age: Int)
+
+    val mutablePerson = new MutablePerson("Bob", 25)
+    mutablePerson.age = 26
+
+    println(mutablePerson) // Output: MutablePerson@<hashcode>(Bob,26)
   }
 }
