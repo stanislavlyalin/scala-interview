471c96f6383d2b5f2862afc69c1503bad3ce7053
lyalin <lyalin@retail-soft.pro>
2024-06-14 19:45:48 +0300

    Поиск подмассива с заданной суммой

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,28 +1,34 @@
 object Main {
 
-  // Дан неотсортированный массив целых чисел. Найдите длину самой длинной последовательности
-  // последовательных чисел, которая может быть создана из элементов массива.
+  // Дан массив целых чисел и целое число S. Найдите такой подмассив, сумма элементов которого равна S.
+  // Если таких подмассивов несколько, верните любой из них. Если такого подмассива нет, верните пустой массив.
 
-  def maxSequenceLength(arr: List[Int]): Int = {
-    def maxSequencies(pairs: List[(Int, Int)], lengths: List[Int]): Int = {
-      if (pairs.length <= 1) Math.max(1, lengths.maxOption.getOrElse(1))
-      else {
-        val l = pairs.takeWhile { case (a, b) => b - a  == 1 }
-        val b = pairs.takeWhile { case (a, b) => b - a  != 1 }
-        maxSequencies(pairs.drop(l.length + b.length), lengths :+ (l.length + 1))
-      }
+  private def removeAt(array: List[Int], idx: Int): List[Int] = {
+    val (l, r) = array.splitAt(idx)
+    l ++ r.tail
+  }
+
+  private def subArrayForSum(array: List[Int], s: Int): List[Int] = {
+    def recF(subArray: List[Int], reminder: List[Int]): List[Int] = {
+      reminder.find(v => subArray.sum + v == s).map(v => subArray :+ v).orElse {
+        reminder
+          .zipWithIndex
+          .map { case (v, idx) =>
+            val newSubArray = subArray :+ v
+            val newReminder = removeAt(reminder, idx)
+            recF(newSubArray, newReminder)
+          }.find(_.sum == s)
+      }.getOrElse(List.empty[Int])
     }
 
-    val sortedArr = arr.sorted.distinct
-    val pairs = sortedArr.dropRight(1).zip(sortedArr.tail)
-
-    maxSequencies(pairs, List.empty[Int])
+    recF(List.empty[Int], array)
   }
 
   def main(args: Array[String]): Unit = {
-    assert(maxSequenceLength(List(100, 4, 200, 1, 3, 2)) == 4)
-    assert(maxSequenceLength(List(0, 3, 7, 2, 5, 8, 4, 6, 0, 1)) == 9)
-    assert(maxSequenceLength(List(10, 5, 12, 3, 55, 2, 1, 11)) == 3)
-    assert(maxSequenceLength(List(1, 9, 3, 10, 2, 20, 4)) == 4)
+    assert(subArrayForSum(List(1, 2, 3, 4, 5), 9).sum == 9)
+    assert(subArrayForSum(List(1, 2, 3, 7, 5), 12).sum == 12)
+    assert(subArrayForSum(List(1, 2, 3, 4, 5), 15).sum == 15)
+    assert(subArrayForSum(List(1, 2, 3, 4, 5), 20).sum == 0)
+    assert(subArrayForSum(List(-1, -2, 4, 7, 1, -3, 2), 5).sum == 5)
   }
 }
