4e19a2c5ba21b308d990546476608cf872b03396
stanislavlyalin <lyalinstas@gmail.com>
2024-06-21 16:26:53 +0300

    Для длительных блокирующих задач в cats-effect лучше использовать IO.blocking, так как в этом случае создаётся отдельный пул потоков для блокирующих задач

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,44 +1,24 @@
+import cats.effect.IO
 import cats.effect.unsafe.implicits.global
-import cats.effect.{IO, Resource}
 import cats.syntax.all._
-
-import java.util.concurrent.{ExecutorService, Executors}
-import scala.concurrent.ExecutionContext
 
 object Main {
   def main(args: Array[String]): Unit = {
-
-    // Создаем Resource для фиксированного пула потоков
-    def createThreadPool: Resource[IO, (ExecutionContext, ExecutorService)] = {
-      Resource.make {
-        IO {
-          val executor = Executors.newFixedThreadPool(4)
-          val ec       = ExecutionContext.fromExecutor(executor)
-          (ec, executor)
-        }
-      } { case (_, executor) =>
-        IO(executor.shutdown())
-      }
-    }
-
     // Длительная задача
-    def longTask(id: Int)(implicit ec: ExecutionContext): IO[Int] = IO {
+    def longTask(id: Int): IO[Int] = IO.blocking {
       println(s"Запуск долгой задачи $id")
       Thread.sleep(5000) // Имитация длительной работы
       println(s"Завершение долгой задачи $id")
       id
-    }.evalOn(ec)
+    }
 
     // Короткая задача
-    def shortSadTask(implicit ec: ExecutionContext): IO[Unit] = IO {
-      println("Я только хочу напечатать 'Привет, мир', но вынуждена ждать завершения 4-х долгих задач")
-    }.evalOn(ec)
+    def shortFunnyTask: IO[Unit] = IO {
+      println("Я только хочу напечатать 'Привет, мир', и мне больше не нужно ждать! :)")
+    }
 
-    createThreadPool.use { case (ec, _) =>
-      implicit val implicitEc: ExecutionContext = ec
-      for {
-        _ <- List(longTask(1), longTask(2), longTask(3), longTask(4), shortSadTask).parSequence
-      } yield ()
-    }.unsafeRunSync()
+    (for {
+      _ <- List(longTask(1), longTask(2), longTask(3), longTask(4), shortFunnyTask).parSequence
+    } yield ()).unsafeRunSync()
   }
 }
