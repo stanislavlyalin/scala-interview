85a1ab2e3ac613d97726239c247cd4cbeeeb6b37
stanislavlyalin <lyalinstas@gmail.com>
2024-09-06 17:32:20 +0300

    Волокна (fibers) в cats-effect

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,30 +4,18 @@ import cats.effect.unsafe.implicits.global
 import scala.concurrent.duration.DurationInt
 
 object Main {
-  // "Корутина" A
-  private def coroutineA: IO[Unit] = for {
-    _ <- IO.println("Coroutine A: Начало")
-    _ <- IO.sleep(1.second) // Приостанавливаем выполнение на 1 секунду
-    _ <- IO.println("Coroutine A: Продолжение после паузы")
-    _ <- IO.sleep(1.second)
-    _ <- IO.println("Coroutine A: Завершение")
-  } yield ()
-
-  // "Корутина" B
-  private def coroutineB: IO[Unit] = for {
-    _ <- IO.println("Coroutine B: Начало")
-    _ <- IO.sleep(500.millis) // Приостанавливаем выполнение на 500 миллисекунд
-    _ <- IO.println("Coroutine B: Продолжение после паузы")
-    _ <- IO.sleep(500.millis)
-    _ <- IO.println("Coroutine B: Завершение")
-  } yield ()
+  // Функция для работы с fiber
+  private def task(id: Int): IO[Unit] =
+    IO.println(s"Task $id started") *>
+      IO.sleep(1.second) *>
+      IO.println(s"Task $id finished")
 
   def main(args: Array[String]): Unit = {
     val program = for {
-      fiberA <- coroutineA.start // Запускаем coroutineA асинхронно
-      fiberB <- coroutineB.start // Запускаем coroutineB асинхронно
-      _      <- fiberA.join      // Ждем завершения coroutineA
-      _      <- fiberB.join      // Ждем завершения coroutineB
+      fiber1 <- task(1).start // Запуск задачи как fiber
+      fiber2 <- task(2).start
+      _      <- fiber1.join   // Ожидание завершения первой fiber
+      _      <- fiber2.join   // Ожидание завершения второй fiber
     } yield ()
 
     program.unsafeRunSync()
